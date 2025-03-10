10a02713142f7284855f9077ef00bae8e413d34b
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2025-03-10 13:28:40 +0200

    Updated models, serializers, views, urls, and added migration and fixtures

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
-from cinema.views import movie_list, movie_detail
-
-urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-]
+from . import views
 
 app_name = "cinema"
+
+urlpatterns = [
+    path('movies/', views.MovieViewSet.as_view({'get': 'list', 'post': 'create'}), name='movie-list'),
+    path('movies/<int:pk>/', views.MovieViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='movie-detail'),
+    path('genres/', views.GenreViewSet.as_view({'get': 'list', 'post': 'create'}), name='genre-list'),
+    path('actors/', views.ActorViewSet.as_view({'get': 'list', 'post': 'create'}), name='actor-list'),
+    path('cinema_halls/', views.CinemaHallViewSet.as_view({'get': 'list', 'post': 'create'}), name='cinema-hall-list'),
+]
